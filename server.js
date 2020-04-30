197fb58c256ebe093deed583c4738f2a3b8aa37c
MarianoRamborger <Marianoramborger@gmail.com>
2020-04-30 09:52:12 -0300

    v0.1

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+const bodyParser = require('body-parser')
+let nodemailer = require('nodemailer')
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+
+app.post ('/newsletter', (req, res) => {
+
+  console.log(req.body) 
+  res.send(req.body)
+
+
+    let sender = nodemailer.createTransport({
+
+        host: "smtp.gmail.com",
+        auth: {
+            type: "login", // default
+            user: "laptopsdelivery@gmail.com",
+            pass: "pagolargo1738"
+         
+        }
+    })
+
+    let mailOptions = {
+        from: 'laptopsdelivery@gmail.com',
+        to: 'laptopsdelivery@gmail.com',
+        subject: "Nuevo suscriptor a la newsLetter",
+        text: `El suscriptor se llama ${req.body.name} y su mail es ${req.body.email}`
+
+    }
+
+    sender.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+
+    });
+} )
+
+
+
+
+app.get('/', (req, res) => res.send('Hello World2222!'))
+
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
